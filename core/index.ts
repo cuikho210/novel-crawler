2d2181d51288427e39ea605ab230fd44360f14bb
cuikho210 <tonykms210@gmail.com>
2024-06-18 04:07:25 +0700

    concurrency fetch :v

--- core/index.ts
+++ core/index.ts
@@ -1,25 +1,33 @@
-import { getAllChapterUrls, getChapter } from "../crawlers/truyencv";
+import { getChapter, fetchBookData } from "../crawlers/metruyencv";
 import { exportToEpub } from "../exporters/to-epub";
 import { resolve } from "path";
 import type { Chapter } from "../crawlers/types";
 
-const novelName = 'tham-uyen-nu-than-vuc-sau-nu-than';
-const title = 'Thâm Uyên Nữ Thần';
-const author = 'Đằng La Vi Chi';
-const chapters = await getChapters(novelName);
-const exportPath = resolve(__dirname, novelName + '.epub');
+const bookId = 104000;
+const bookInfo = await fetchBookData(bookId);
+const chapters = await getChapters();
+
+const title = bookInfo.extra.book.name;
+const author = 'Cuikho210';
+const exportPath = resolve(__dirname, bookInfo.extra.book.slug + '.epub');
 
 exportToEpub({ title, author, chapters, }, exportPath);
 
-export async function getChapters(novelName: string): Promise<Chapter[]> {
-	const urls = await getAllChapterUrls(novelName);
+export async function getChapters(): Promise<Chapter[]> {
 	const chapters: Chapter[] = [];
+	const promises = <Promise<void>[]>[];
 
-	for (let i = 0; i < urls.length; i++) {
-		const url = urls[i];
-		const chapter = await getChapter(url, i);
-		chapters.push(chapter);
+	for (const chap of bookInfo.data) {
+		promises.push(new Promise(async (resolve) => {
+			const uri = `/${bookInfo.extra.book.slug}/chuong-${chap.index}`;
+			const chapter = await getChapter(uri, chap.index, chap.name);
+			chapters.push(chapter);
+			resolve();
+		}));
 	}
 
-	return chapters;
+	await Promise.all(promises);
+
+	const sorted = chapters.sort((a, b) => a.index - b.index);
+	return sorted;
 }
